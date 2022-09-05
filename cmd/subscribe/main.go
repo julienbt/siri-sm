04b212020e7a09f966f8c26ed2922f3f181ce94f
Julien Bt <julienbt@protonmail.com>
2022-09-05 09:02:06 +0200

    Start subscribe executable

--- cmd/subscribe/main.go
+++ cmd/subscribe/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"runtime"
+
+	"github.com/julienbt/siri-sm/internal/config"
+	"github.com/julienbt/siri-sm/internal/subscribe"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("Hello from subscribe")
+
+	logger := getLogger()
+
+	var cfg config.ConfigSubscribe
+	err := envconfig.Process("SIRISM_SUBSCRIBE", &cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	subscribeResp, err := subscribe.Subscribe(cfg, logger)
+	_ = err
+	_ = subscribeResp
+
+}
+
+func getLogger() *logrus.Entry {
+	return logrus.WithFields(logrus.Fields{
+		"app":     "subscribe",
+		"runtime": runtime.Version(),
+	})
 }
